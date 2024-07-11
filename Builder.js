ecd7aa85b74dbdcd0abbdd05476c89b25e9dda69
burakbehlull <burakyabgu@gmail.com>
2024-07-12 02:26:54 +0300

    feat: Add event list

---
+++ Builder.js
@@ -0,0 +1,19 @@
+const Events = {
+    Join: 'join',
+    Leave: 'leave',
+    Chat: 'chat',
+    Kicked: 'kicked',
+    BallKick: 'ball_kick',
+    Start: 'start',
+    Stop: 'stop',
+    Tick: 'tick',
+    Pause: 'pause',
+    Unpuase: 'unpause',
+    Activity: 'activity',
+    TeamChange: 'team_change',
+    PositionsReset: 'positions_reset',
+    TeamVictory: 'team_victory',
+    TeamGoal: 'team_goal',
+    StadiumChange: 'stadium_change',
+    RoomLink:'room_link'
+}
